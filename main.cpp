9c31e120c6db2d77289c0bc1310d11cd7ceeac3e
Claudomiro Sales <claudomiro.sales@gmail.com>
2016-01-21 19:14:56 -0300

    Testando a biblioteca Merssener-twister
    #include "Rand.h"
    
    int main(int argc, char **argv)
    {
        
        Rand * rand_ = new Rand( );
        rand_->SetSeed( );
        
        cout << rand_->Uniform( ) << '\n';

--- main.cpp
+++ main.cpp
@@ -5,14 +5,26 @@ using std::cout;
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 
+#include "Rand.h"
+
 int main(int argc, char **argv)
 {
+    
+    Rand * rand_ = new Rand( );
+    rand_->SetSeed( );
+    
+    cout << rand_->Uniform( ) << '\n';
+  
+    
+    /*
+    //Abaixo usando o Rand da biblioteca padrão
+    ///////////////////////////////////////
     int idadedaDanny, iTentativa;
    
-    /* initialize random seed: */
+    // initialize random seed: //
     srand (time(NULL));
     
-    /* generate um número entre 1 e 25: */
+    // generate um número entre 1 e 25: //
     idadedaDanny = rand() % 25 + 1;
         
     
@@ -26,6 +38,8 @@ int main(int argc, char **argv)
    } while (idadedaDanny != iTentativa);
     
     cout << "Parabens!\n";
+    
+    */
     
 	return 0;
 }
